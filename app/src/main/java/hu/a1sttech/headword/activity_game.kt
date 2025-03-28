d79ce78f018fc370a104d58960d94f21734daceb
csalamade <40370960+csalamade@users.noreply.github.com>
2025-03-28 11:55:57 +0100

    Initial

---
+++ app/src/main/java/hu/a1sttech/headword/activity_game.kt
@@ -0,0 +1,25 @@
+package hu.a1sttech.headword
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.content.pm.ActivityInfo
+
+class activity_game : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_game)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        // A kijelzőt fektetett módba állítjuk
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+
+        setContentView(R.layout.activity_game)
+    }
+}
