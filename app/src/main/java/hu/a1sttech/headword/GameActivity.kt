273cb409d80fa5493692344990d504a37795721e
csalamade <40370960+csalamade@users.noreply.github.com>
2025-03-28 16:02:40 +0100

    Képernyő ébren tartása játék közben

--- app/src/main/java/hu/a1sttech/headword/GameActivity.kt
+++ app/src/main/java/hu/a1sttech/headword/GameActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.content.pm.ActivityInfo
+import android.view.WindowManager
 import android.widget.Button
 import android.widget.TextView
 
@@ -28,6 +29,9 @@ class GameActivity : AppCompatActivity() {
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
 
         setContentView(R.layout.activity_game)
+
+        // Képernyő ébren tartása játék közben
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
         val category = intent.getStringExtra("category")  // Kategória neve
         val wordCategories = parseJson(this)  // JSON beolvasása
