dfd2ed3c12c33713af85a73706560e5e79714dc5
csalamade <40370960+csalamade@users.noreply.github.com>
2025-03-28 19:02:29 +0100

    Időzitő műkődik

--- app/src/main/java/hu/a1sttech/headword/GameActivity.kt
+++ app/src/main/java/hu/a1sttech/headword/GameActivity.kt
@@ -6,12 +6,20 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.content.pm.ActivityInfo
+import android.os.CountDownTimer
 import android.view.WindowManager
 import android.widget.Button
 import android.widget.TextView
 
 class GameActivity : AppCompatActivity() {
 
+    //visszaszámláló változói
+
+    private lateinit var countDownTimer: CountDownTimer
+    private val totalTimeInMillis = 30000L // 30 másodperc
+    private val countDownInterval = 1000L // 1 másodpercenként frissít
+
+    //json ból olvasás változói
     private lateinit var words: List<String>  // A szavak listája
     private var currentWordIndex = 0  // Melyik szónál járunk
 
@@ -33,6 +41,8 @@ class GameActivity : AppCompatActivity() {
         // Képernyő ébren tartása játék közben
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
+        startCountDown()
+
         val category = intent.getStringExtra("category")  // Kategória neve
         val wordCategories = parseJson(this)  // JSON beolvasása
         words = wordCategories[category] ?: listOf("Nincs szó elérhető")  // Kategória szavai
@@ -47,7 +57,39 @@ class GameActivity : AppCompatActivity() {
         nextButton.setOnClickListener {
             currentWordIndex = (currentWordIndex + 1) % words.size  // Körforgás
             wordTextView.text = words[currentWordIndex]
+            restartTimer()
         }
 
+    }
+
+    private fun startCountDown() {
+        countDownTimer = object : CountDownTimer(totalTimeInMillis, countDownInterval) {
+            override fun onTick(millisUntilFinished: Long) {
+                // Minden másodpercben frissíti a felületet
+                val secondsRemaining = millisUntilFinished / 1000
+                findViewById<TextView>(R.id.timerTextView).text =
+                    "Hátralévő idő: $secondsRemaining másodperc"
+            }
+
+            override fun onFinish() {
+                // Amikor lejár az idő
+                findViewById<TextView>(R.id.timerTextView).text = "Lejárt az idő!"
+            }
+        }.start()
+    }
+
+    private fun restartTimer() {
+        // Meglévő timer leállítása
+        countDownTimer?.cancel()
+
+        // Új timer indítása
+        startCountDown()
+    }
+
+
+    //timer leállítása a memória szívárgás elkerülése érdekében
+    override fun onDestroy() {
+        super.onDestroy()
+        countDownTimer?.cancel()
     }
 }
